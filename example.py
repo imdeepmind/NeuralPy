9e42afd2a38a79b3eff96fbb924ce022354dc361
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-03 16:53:32 +0530

    added the code for dense layer

--- example.py
+++ example.py
@@ -1,3 +1,10 @@
 from neuralpy.layers import Dense
 
-dense = Dense()
+
+input = [[1,2,3], [4,5,6]]
+
+dense_layer = Dense(3, 1)
+dense_layer.forward(input)
+
+print("Output of the network", dense_layer.output)
+print("Weights of the network", dense_layer.get_weights())
--- neuralpy/layers/dense.py
+++ neuralpy/layers/dense.py
@@ -1,5 +1,12 @@
 import numpy as np
 
 class Dense:
-	def __init__(self):
-		print("Working...")
+	def __init__(self, n_inputs, n_nodes):
+		self.__weights = np.random.randn(n_inputs, n_nodes)
+		self.__bias = np.zeros((1, n_nodes), dtype=np.float)
+	
+	def forward(self, x):
+		self.output = np.dot(x, self.__weights) + self.__bias
+
+	def get_weights(self):
+		return {'weights': self.__weights, 'bias': self.__bias}
