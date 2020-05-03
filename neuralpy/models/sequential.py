bd3a983374f2331a0256998a4e6ebcc537d1a36a
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-03 23:47:39 +0530

    fixed some issues

--- neuralpy/models/sequential.py
+++ neuralpy/models/sequential.py
@@ -9,14 +9,14 @@ class Sequential:
 
 	def summary(self):
 		lines = "========================================================================\n"
-		n_params = 1
+		n_params = 0
 
 		for layer in self.__layers:
 			layer_details = layer.get_details()
 			n_params += layer_details["n_params"]
 
 
-			lines += f"Layer Name: {layer_details['name']}	Inputs: {layer_details['n_inputs']}	Nodes: {layer_details['n_nodes']}	Parameters: {layer_details['n_params']}\n"
+			lines += f"Layer Name: {layer_details['name']}	Inputs: {layer_details['n_inputs']}	Outputs: {layer_details['n_nodes']}	Parameters: {layer_details['n_params']}\n"
 			lines += "========================================================================\n"
 
 		lines += f"\nTotal Trainable Parameters: {n_params}\n"
