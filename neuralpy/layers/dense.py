425d18d643f5ddfc0b80a04bf0265578447636ff
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-07 00:04:54 +0530

    some code cleanup

--- neuralpy/layers/dense.py
+++ neuralpy/layers/dense.py
@@ -2,18 +2,23 @@ from torch.nn import Linear
 
 class Dense:
 	def __init__(self, n_nodes, n_inputs=None, bias=True, name=None):
+		# Check the n_nodes field
 		if not n_nodes or n_nodes <= 0:
 			raise ValueError("Please provide a valid n_nodes")
 
+		# Checking the n_input field, it is a optional field
 		if n_inputs is not None and n_inputs <= 0:
 			raise ValueError("Please provide a valid n_inputs")
 
+		# Checking the bias field, this is also optional, default to True
 		if not (bias == True or bias == False):
 			raise ValueError("Please provide a valid bias")
 
+		# Checking the name field, this is an optional field, if not provided generates a unique name for the layer
 		if name is not None and not (isinstance(name, str) and name is not ""):
 			raise ValueError("Please provide a valid name")
 
+		# Storing the data
 		self.__n_inputs = n_inputs
 		self.__n_nodes = n_nodes
 
@@ -21,14 +26,15 @@ class Dense:
 		self.__name = name
 
 	def get_input_dim(self, prev_input_dim):
+		# Checking if n_inputs is there or not, not overwriting the n_input field
 		if not self.__n_inputs:
 			self.__n_inputs = prev_input_dim
 
 	def get_layer(self):
+		# Returning all the details of the layer
 		return {
 			'n_inputs': self.__n_inputs,
 			'n_nodes': self.__n_nodes,
-			'n_params': self.__n_nodes * self.__n_inputs + self.__n_nodes,
 			'name': self.__name,
 			'type': 'Dense',
 			'layer': Linear,
--- neuralpy/models/sequential.py
+++ neuralpy/models/sequential.py
@@ -7,47 +7,67 @@ class Sequential():
 	__layers = []
 	__model = None
 	__build = False
-	__prev_output_dim = 0
 
 	def __init__(self):
 		super(Sequential, self).__init__()
 
 	def __generate_layer_name(self, type, index):
+		# Generating a unique name for the layer
 		return f"{type.lower()}_layer_{index+1}"
 
 	def add(self, layer):
+		# If we already built the model, then we can not a new layer
 		if (self.__build):
 			raise Exception("You have built this model already, you can not make any changes in this model")
 
+		# We need to pass the layer
 		if not layer:
 			raise Exception("You need to pass a layer")
 
+		# Finally adding the layer for layers array
 		self.__layers.append(layer)
 
 	def build(self):
 		layers = []
+		prev_output_dim = 0
 
+		# Iterating through the layers
 		for index, layer_ref in enumerate(self.__layers):
-			if self.__prev_output_dim is not 0:
-				layer_ref.get_input_dim(self.__prev_output_dim)
+			# Generating n_input if not present
+			if prev_output_dim is not 0:
+				layer_ref.get_input_dim(prev_output_dim)
 
+			# Getting the details of the layer
 			layer_details = layer_ref.get_layer()
 
-			layer = layer_details["layer"](**layer_details["keyword_arguments"])
+			layer_name = layer_details["name"]
+			layer_type = layer_details["type"]
+			layer_nodes = layer_details["n_nodes"]
+			layer_arguments = layer_details["keyword_arguments"]
+			layer_function_ref = layer_details["layer"]
 
-			name = layer_details["name"]
+			# If layer does not have name, then creating a unique name
+			if not layer_name:
+				layer_name = self.__generate_layer_name(layer_type, index)
 
-			if not name:
-				name = self.__generate_layer_name(layer_details["type"], index)
+			# Calling a layer function with arguments to make the layer
+			layer = layer_function_ref(**layer_arguments) 
 
-			layers.append((name, layer))
+			# Appending the layer to layers array
+			layers.append((layer_name, layer))
 
-			self.__prev_output_dim = layer_details["n_nodes"]
+			# Storing the n_nodes to calculate the input dim of next layer
+			prev_output_dim = layer_nodes
 
+
+		# Making the pytorch model using nn.Sequential
 		self.__model = nn.Sequential(OrderedDict(layers))
+
+		# Chanding the build status to True, so we can not make any changes
 		self.__build = True
 
 	def summary(self):
+		# Printing the model summary using pytorch model
 		if self.__build:
 			print(self.__model)
 			print("Total Number of Parameters: ", sum(p.numel() for p in self.__model.parameters()))
