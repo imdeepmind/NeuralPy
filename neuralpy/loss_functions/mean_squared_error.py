b4818ff022141a8530bd765dc43336f7d1fd6c9d
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-09 23:08:35 +0530

    added mse

--- neuralpy/loss_functions/mean_squared_error.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def mean_squared_error(y, y_pred):
-	return (np.sum(y - y_pred)**2) / len(y)
---
+++ neuralpy/loss_functions/mse.py
@@ -0,0 +1,15 @@
+import torch.nn.MSELoss as _MSELoss
+
+class MSELoss:
+	def __init__(self, reduction='mean'):
+		self.__reduction = reduction
+
+	def get_loss_function(self):
+		return {
+			'loss_function': _MSELoss,
+			'keyword_arguments': {
+				'size_average': None, # Deprecated
+				'reduce': None, # Deprecated
+				'reduction': self.__reduction
+			}
+		}
