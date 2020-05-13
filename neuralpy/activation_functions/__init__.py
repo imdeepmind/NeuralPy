7ab3d8cd88868737205bff80ad805f26db9c1b92
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-13 22:28:57 +0530

    splitted the activation functions to multiple files

--- neuralpy/activation_functions/__init__.py
+++ neuralpy/activation_functions/__init__.py
@@ -1,1 +1,3 @@
-from .activation_functions import ReLU, Sigmoid, Softmax
+from .relu import ReLU
+from .sigmoid import Sigmoid
+from .softmax import Softmax
---
+++ neuralpy/activation_functions/softmax.py
@@ -0,0 +1,27 @@
+from torch.nn import Softmax as _Softmax
+
+class Softmax:
+	def __init__(self, dim=None, name=None):
+		# Checking the name field, this is an optional field, if not provided generates a unique name for the layer
+		if name is not None and not (isinstance(name, str) and name is not ""):
+			raise ValueError("Please provide a valid name")
+
+		self.__name = name
+		self.__dim = dim
+
+	def get_input_dim(self, prev_input_dim):
+		# Softmax does not need to n_input, so returning None
+		return None
+
+	def get_layer(self):
+		# Returning all the details of the layer
+		return {
+			'n_inputs': None,
+			'n_nodes': None,
+			'name': self.__name,
+			'type': 'Softmax',
+			'layer': _Softmax,
+			"keyword_arguments": {
+				'dim': self.__dim
+			}
+		}
