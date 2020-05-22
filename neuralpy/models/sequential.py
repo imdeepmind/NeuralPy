72126d99634d02ecfae6e047a3a93ee430fdcc63
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-22 23:30:08 +0530

    refactored the build method

--- neuralpy/models/sequential.py
+++ neuralpy/models/sequential.py
@@ -50,53 +50,8 @@ class Sequential(SequentialHelper):
 		self.__layers.append(layer)
 
 	def build(self):
-		# Storing the layer here to build the Sequentuial layer
-		layers = []
-
-		# Strong the output dimension, for the next layer, we need this to calculate the next input layer dim
-		prev_output_dim = 0
-
-		# Iterating through the layers
-		for index, layer_ref in enumerate(self.__layers):
-
-			# Generating n_input if not present
-			if prev_output_dim is not 0:
-				# For each layer, we have this method that returns the new input layer for next dim
-				# based on the previous output dim
-				layer_ref.get_input_dim(prev_output_dim)
-
-			# Getting the details of the layer using the get_layer method
-			layer_details = layer_ref.get_layer()
-
-			# Stroing the layer details
-			layer_name = layer_details["name"]
-			layer_type = layer_details["type"]
-			layer_nodes = layer_details["n_nodes"]
-			layer_arguments = layer_details["keyword_arguments"]
-
-			# Here we are just storing the ref, not the initialized layer 
-			layer_function_ref = layer_details["layer"]
-
-			# If layer does not have name, then creating a unique name
-			if not layer_name:
-				# This method generates a unique layer name based on layer type and index
-				layer_name = self._generate_layer_name(layer_type, index)
-
-			# If layer_arguments is not None, then the layer accepts some parameters to initialize 
-			if layer_arguments is not None:
-				# Here passing the layer_arguments to the layer reference to initialize the layer
-				layer = layer_function_ref(**layer_arguments) 
-			else:
-				# This layer does not need layer_arguments so not passing anything
-				layer = layer_function_ref() 
-
-			# Appending the layer to layers array
-			layers.append((layer_name, layer))
-
-			# Checking layer_nodes value against some condition, and then storing the n_nodes to calculate the input dim of next layer 
-			if layer_nodes is not None and layer_nodes >= 0:
-				prev_output_dim = layer_nodes
-
+		# Building the layers from the layer refs and details
+		layers = self._build_layer_from_ref_and_details(self.__layers)
 
 		# Making the pytorch model using nn.Sequential
 		self.__model = nn.Sequential(OrderedDict(layers))
--- neuralpy/models/sequential_helper.py
+++ neuralpy/models/sequential_helper.py
@@ -2,6 +2,10 @@ class SequentialHelper:
 	def __init__(self):
 		pass
 
+	def __generate_layer_name(self, layer_type, index):
+		# Generating a unique name for the layer
+		return f"{layer_type.lower()}_layer_{index+1}"
+
 	def _is_valid_layer(self, layer):
 		# if the layer is none, returning False
 		if not layer:
@@ -132,6 +136,52 @@ class SequentialHelper:
 		except KeyError:
 			return False
 
-	def _generate_layer_name(self, layer_type, index):
-		# Generating a unique name for the layer
-		return f"{layer_type.lower()}_layer_{index+1}"
+	def _build_layer_from_ref_and_details(self, layer_refs):
+		# Storing the layer here to build the Sequentuial layer
+		layers = []
+
+		# Strong the output dimension, for the next layer, we need this to calculate the next input layer dim
+		prev_output_dim = 0
+
+		# Iterating through the layers
+		for index, layer_ref in enumerate(layer_refs):
+
+			# Generating n_input if not present
+			if prev_output_dim is not 0:
+				# For each layer, we have this method that returns the new input layer for next dim
+				# based on the previous output dim
+				layer_ref.get_input_dim(prev_output_dim)
+
+			# Getting the details of the layer using the get_layer method
+			layer_details = layer_ref.get_layer()
+
+			# Stroing the layer details
+			layer_name = layer_details["name"]
+			layer_type = layer_details["type"]
+			layer_nodes = layer_details["n_nodes"]
+			layer_arguments = layer_details["keyword_arguments"]
+
+			# Here we are just storing the ref, not the initialized layer 
+			layer_function_ref = layer_details["layer"]
+
+			# If layer does not have name, then creating a unique name
+			if not layer_name:
+				# This method generates a unique layer name based on layer type and index
+				layer_name = self.__generate_layer_name(layer_type, index)
+
+			# If layer_arguments is not None, then the layer accepts some parameters to initialize 
+			if layer_arguments is not None:
+				# Here passing the layer_arguments to the layer reference to initialize the layer
+				layer = layer_function_ref(**layer_arguments) 
+			else:
+				# This layer does not need layer_arguments so not passing anything
+				layer = layer_function_ref() 
+
+			# Appending the layer to layers array
+			layers.append((layer_name, layer))
+
+			# Checking layer_nodes value against some condition, and then storing the n_nodes to calculate the input dim of next layer 
+			if layer_nodes is not None and layer_nodes >= 0:
+				prev_output_dim = layer_nodes
+
+		return layers
