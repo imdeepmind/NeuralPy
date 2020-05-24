fd6f85b28c115bc3a5ea5b5c7e040384305389a5
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-25 00:06:08 +0530

    updated the init py and setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,39 @@
 from setuptools import setup
 
-def get_readme():
-	with open("README.md", "r") as f:
-		return f.read()
+long_description = """
+NeuralPy
+
+A Keras like deep learning library works on top of PyTorch
+
+Check the docs for more info: https://neuralpy.imdeepmind.com/
+"""
 
 setup(
-	name="neuralpy",
+	name="neuralpy-torch",
 	version="0.0.1",
-	description=" A Keras like deep learning library works on top of PyTorch",
-	long_description=get_readme(),
-	long_description_content_type="text/markdown",
+	description="A Keras like deep learning library works on top of PyTorch",
+	long_description=long_description,
 	url="https://github.com/imdeepmind/NeuralPy",
 	author="Abhishek Chatterjee",
 	author_email="abhishek.chatterjee97@protonmail.com",
 	license="MIT",
 	classifiers=[
-		"Development Status :: Unstable",
+		"Development Status :: 1 - Planning",
 		"Intended Audience :: Developers",
 		"Intended Audience :: Education",
 		"Intended Audience :: Science/Research",
 		"License :: OSI Approved :: MIT License",
 		"Programming Language :: Python :: 3",
 		"Programming Language :: Python :: 3.6",
-		"Programming Language :: Python :: 3.7",
-		"Topic :: Software Development :: Libraries"
-		"Topic :: Software Development :: Libraries :: Python Modules"
+		"Programming Language :: Python :: 3.7"
 	],
-	packages=["neuralpy"],
+	packages=[
+		"neuralpy",
+		"neuralpy.activation_functions",
+		"neuralpy.layers",
+		"neuralpy.loss_functions",
+		"neuralpy.optimizer",
+		"neuralpy.regulariziers"
+	],
 	include_package_data=True
 )
