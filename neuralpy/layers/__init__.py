670c8a37063c6e7361a5a2e0824a68995f5eae2c
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-28 23:03:14 +0530

    fixed some issues, added doc string

--- neuralpy/layers/__init__.py
+++ neuralpy/layers/__init__.py
@@ -1,1 +1,10 @@
+"""Layers are the building blocks of a Neural Network.
+A complete Neural Network model consists of several layers.
+A Layer is a function that receives a tensor as output,
+computes something out of it, and finally outputs a tensor.
+
+NeuralPy currently supports only one type of Layer and
+that is the Dense layer.
+"""
+
 from .dense import Dense
--- neuralpy/layers/dense.py
+++ neuralpy/layers/dense.py
@@ -1,8 +1,33 @@
 from torch.nn import Linear
 
-
 class Dense:
+    """
+        A Dense is a normal densely connected Neural Network.
+        It performs a linear transformation of the input.
+
+        To learn more about Dense layers, please check PyTorch
+        documentation at https://pytorch.org/docs/stable/nn.html?highlight=linear
+
+        Supported Arguments:
+            n_nodes: (Integer) Size of the output sample
+            n_inputs: (Integer) Size of the input sample,
+                no need for this argument layers except the initial layer.
+            bias: (Boolean) If true then uses the bias, Defaults to `true`
+            name: (String) Name of the layer, if not provided then
+                automatically calculates a unique name for the layer
+    """
     def __init__(self, n_nodes, n_inputs=None, bias=True, name=None):
+        """
+            __init__ method for the Dense layer
+
+            Supported Arguments:
+                n_nodes: (Integer) Size of the output sample
+                n_inputs: (Integer) Size of the input sample,
+                    no need for this argument layers except the initial layer.
+                bias: (Boolean) If true then uses the bias, Defaults to `true`
+                name: (String) Name of the layer, if not provided then
+                    automatically calculates a unique name for the layer
+        """
         # Checking the n_nodes field
         if not n_nodes or not isinstance(n_nodes, int) or n_nodes <= 0:
             raise ValueError("Please provide a valid n_nodes")
@@ -15,8 +40,9 @@ class Dense:
         if not isinstance(bias, bool):
             raise ValueError("Please provide a valid bias")
 
-        # Checking the name field, this is an optional field, if not provided generates a unique name for the layer
-        if name and not (isinstance(name, str) and name is not ""):
+        # Checking the name field, this is an optional field,
+        # if not provided generates a unique name for the layer
+        if name and isinstance(name, str) and len(name) <= 0:
             raise ValueError("Please provide a valid name")
 
         # Storing the data
@@ -27,11 +53,23 @@ class Dense:
         self.__name = name
 
     def get_input_dim(self, prev_input_dim):
+        """
+            This method calculates the input shape for layer based on previous output layer.
+
+            This method is used by the NeuralPy Models, for building the models.
+            No need to call this method for using NeuralPy.
+        """
         # Checking if n_inputs is there or not, not overwriting the n_input field
         if not self.__n_inputs:
             self.__n_inputs = prev_input_dim
 
     def get_layer(self):
+        """
+            This method returns the details as dict of the layer.
+
+            This method is used by the NeuralPy Models, for building the models.
+            No need to call this method for using NeuralPy.
+        """
         # Returning all the details of the layer
         return {
             'n_inputs': self.__n_inputs,
@@ -40,8 +78,8 @@ class Dense:
             'type': 'Dense',
             'layer': Linear,
             "keyword_arguments": {
-                    'in_features': self.__n_inputs,
-                    'out_features': self.__n_nodes,
-                'bias': self.__bias
+    	           'in_features': self.__n_inputs,
+    	           'out_features': self.__n_nodes,
+    	           'bias': self.__bias
             }
         }
