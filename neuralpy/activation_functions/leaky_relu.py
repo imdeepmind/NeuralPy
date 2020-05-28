9a52e2835c4a80444aaf72ff0e174ffb2674e70c
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-28 23:46:00 +0530

    added some test cases, fixed name validation in leaky rely

--- neuralpy/activation_functions/leaky_relu.py
+++ neuralpy/activation_functions/leaky_relu.py
@@ -6,8 +6,12 @@ class LeakyReLU:
     def __init__(self, negative_slope=0.01, name=None):
         # Checking the name field, this is an optional field,
         # if not provided generates a unique name for the activation function
-        if name and isinstance(name, str) and (name) > 0:
-            raise ValueError("Please provide a valid name")
+        if name is not None:
+        	if isinstance(name, str):
+        		if len(name) <= 0:
+        			raise ValueError("Please provide a valid name")
+        	else:
+        		raise ValueError("Please provide a valid name")
 
         self.__negative_slope = negative_slope
         self.__name = name
--- tests/neuralpy/activation_functions/test_leaky_relu.py
+++ tests/neuralpy/activation_functions/test_leaky_relu.py
@@ -1,1 +1,16 @@
+import pytest
 from neuralpy.activation_functions import LeakyReLU
+
+# Possible values
+negative_slopes = [0.01]
+names = [False, 12, 3.6, -2]
+
+@pytest.mark.parametrize(
+	"negative_slope, name", 
+	[(negative_slope, name) for negative_slope in negative_slopes
+						    for name in names]
+)
+def test_leaky_relu_should_throw_value_error_Exception(negative_slope, name):
+    with pytest.raises(ValueError) as ex:
+        x = LeakyReLU(negative_slope=negative_slope, name=name)
+
