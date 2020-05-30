2209875debe8c097ebec7182e6f666a141515fb3
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-30 13:58:20 +0530

    minor code improvements to the dropout class

--- neuralpy/regulariziers/dropout.py
+++ neuralpy/regulariziers/dropout.py
@@ -1,17 +1,19 @@
 from torch.nn import Dropout as _Dropout
 
-
 class Dropout:
     def __init__(self, p=0.5, name=None):
+        # Checking the field p, it should be a valid prob value
+        if not (isinstance(p, float) and p >= 0.0 and p <= 1.0):
+            raise ValueError("Please provide a valid p value")
         # Checking the name field, this is an optional field,
         # if not provided generates a unique name for the layer
-        if name is not None and not (isinstance(name, str) and name is not ""):
+        if name is not None and not (isinstance(name, str) and name):
             raise ValueError("Please provide a valid name")
 
         self.__p = p
         self.__name = name
 
-    # pylint: disable=no-self-use
+    # pylint: disable=no-self-use,unused-argument
     def get_input_dim(self, prev_input_dim):
         # Dropout does not need to n_input, so returning None
         return None
