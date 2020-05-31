ce94b5b495e4095ab2dec69a6d88b506eb601ede
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-31 13:39:47 +0530

    code improvements to the Sequential class

--- neuralpy/models/sequential.py
+++ neuralpy/models/sequential.py
@@ -3,7 +3,7 @@
 from collections import OrderedDict
 import torch
 
-from .sequential_helper import *
+from .sequential_helper import is_valid_layer, is_valid_optimizer, is_valid_loss_function, build_layer_from_ref_and_details, build_optimizer_from_ref_and_details, build_loss_function_from_ref_and_details, build_history_object_for_training, calculate_accuracy, print_training_progress, print_validation_progress
 
 class Sequential:
     """
@@ -12,13 +12,22 @@ class Sequential:
         In Sequential models, each layer has a single input and output tensor.
 
         Supported Arguments:
-        force_cpu=False: (Boolean) If True, then uses CPU even if CUDA is available
-        training_device=None: (NeuralPy device class) Device that will
-            be used for training predictions
-        random_state: (Integer) Random state for the device
+            force_cpu=False: (Boolean) If True, then uses CPU even if CUDA is available
+            training_device=None: (NeuralPy device class) Device that will
+                be used for training predictions
+            random_state: (Integer) Random state for the device
     """
+
     def __init__(self, force_cpu=False, training_device=None, random_state=None):
-        super(Sequential, self).__init__()
+        """
+            __init__ method for Sequential Model
+
+            Supported Arguments:
+                force_cpu=False: (Boolean) If True, then uses CPU even if CUDA is available
+                training_device=None: (NeuralPy device class) Device that will
+                    be used for training predictions
+                random_state: (Integer) Random state for the device
+        """
         # Initializing some attributes that we need to function
         self.__layers = []
         self.__model = None
@@ -28,11 +37,12 @@ class Sequential:
         self.__metrics = None
 
         # Checking the force_cpu parameter
-        if not (force_cpu == True or force_cpu == False):
+        if not isinstance(force_cpu, bool):
             raise ValueError(
                 f"You have provided an invalid value for the parameter force_cpu")
 
         # Checking the training_device parameter and comparing it with pytorch device class
+        # pylint: disable=no-member
         if training_device and not isinstance(training_device, torch.device):
             raise ValueError("Please provide a valid neuralpy device class")
 
@@ -43,28 +53,44 @@ class Sequential:
         # if force_cpu then using CPU
         # if device provided, then using it
         # else auto detecting the device, if cuda available then using it (default option)
+
+        # there is a issue pylint, because of that, disabling the no-member check
+        # for more info, have the look at the link below
+        # https://github.com/pytorch/pytorch/issues/701
+
         if training_device:
             self.__device = training_device
-        elif force_cpu == True:
+        elif force_cpu:
+            # pylint: disable=no-member
             self.__device = torch.device("cpu")
         else:
             if torch.cuda.is_available():
+                # pylint: disable=no-member
                 self.__device = torch.device("cuda:0")
             else:
+                # pylint: disable=no-member
                 self.__device = torch.device("cpu")
 
         # Setting random state if given
         if random_state:
             torch.manual_seed(random_state)
 
-    def __predict(self, X, batch_size):
+    def __predict(self, X, batch_size=None):
+        """
+            Method for predicting
+
+            Supported Arguments:
+                X: (Numpy Array) Data
+                batch_size=None: (Integer) Batch size for predicting
+        """
         # Calling model.eval as we are evaluating the model only
         self.__model.eval()
 
         # Initializing an empty list to store the predictions
+        # pylint: disable=not-callable,no-member
         predictions = torch.Tensor()
 
-        # Conveting the input X to pytorch Tensor
+        # Converting the input X to PyTorch Tensor
         X = torch.tensor(X)
 
         if batch_size:
@@ -77,7 +103,7 @@ class Sequential:
 
             # Predicting, so no grad
             with torch.no_grad():
-                # Spliting the data into batches
+                # Splitting the data into batches
                 for i in range(0, len(X), batch_size):
                     # Generating the batch from X
                     batch_x = X[i:i+batch_size].float()
@@ -86,6 +112,7 @@ class Sequential:
                     outputs = self.__model(batch_x)
 
                     # Appending the data into the predictions tensor
+                    # pylint: disable=not-callable,no-member
                     predictions = torch.cat((predictions, outputs))
         else:
             # Predicting, so no grad
@@ -100,6 +127,16 @@ class Sequential:
         return predictions
 
     def add(self, layer):
+        """
+            In a Sequential model, the .add() method is responsible
+            for adding a new layer to the model. It accepts a NeuralPy
+            layer class as an argument and builds a model, and based on that.
+            The .add() method can be called as many times as needed. There is no
+            limitation on that, assuming you have enough computation power to handle it.
+
+            Supported Arguments
+                layer: (NeuralPy layer classes) Adds a layer into the model
+        """
         # If we already built the model, then we can not a new layer
         if self.__build:
             raise Exception(
@@ -113,10 +150,20 @@ class Sequential:
         self.__layers.append(layer)
 
     def build(self):
+        """
+            In a Sequential model, the .build() method is responsible for
+            building the PyTorch model from the NeuralPy model.
+
+            After finishing the architecture of the model, the model
+            needed to be build before training.
+
+            Supported Arguments:
+                There is no argument for this model
+        """
         # Building the layers from the layer refs and details
         layers = build_layer_from_ref_and_details(self.__layers)
 
-        # Making the pytorch model using nn.Sequential
+        # Making the PyTorch model using nn.Sequential
         self.__model = torch.nn.Sequential(OrderedDict(layers))
 
         # Transferring the model to device
@@ -125,10 +172,24 @@ class Sequential:
         # Printing a message with the device name
         print("The model is running on", self.__device)
 
-        # Chanding the build status to True, so we can not make any changes
+        # Changing the build status to True, so we can not make any changes
         self.__build = True
 
     def compile(self, optimizer, loss_function, metrics=None):
+        """
+            In Sequential model, compile method is responsible for attaching a loss
+            function and optimizer with the model and this method needs to
+            be called before training.
+
+            The .compile() method internally calls the .build(),
+            so there is no need to call .build().
+
+            Supported Arguments:
+                optimizer: (NeuralPy Optimizer class) Adds a optimizer to the model
+                loss_function: (NeuralPy Loss Function class) Adds a loss function to the model
+                metrics: ([String]) Metrics that will be evaluated by the model.
+                    Currently only supports "accuracy".
+        """
         # To compile a model, first we need to build it, building it first
         if not self.__build:
             # Calling build
@@ -152,7 +213,7 @@ class Sequential:
             loss_function)
 
     def fit(self, train_data, test_data, epochs=10, batch_size=32):
-        # Ectracting the train and test data from the touples
+        # Extracting the train and test data from the tuples
         x_train, y_train = train_data
         x_test, y_test = test_data
 
@@ -182,14 +243,15 @@ class Sequential:
             raise ValueError(
                 "Length of testing Input data and testing output data should be same")
 
-        # Conveting the data into pytorch tensor
+        # Converting the data into PyTorch tensor
+        # pylint: disable=not-callable,no-member
         x_train = torch.tensor(x_train)
         y_train = torch.tensor(y_train)
 
         x_test = torch.tensor(x_test)
         y_test = torch.tensor(y_test)
 
-        # Initializing a dict to store the training progress, can be used for viz purposes
+        # Initializing a dictionary to store the training progress, can be used for viz purposes
         metrics = []
 
         if self.__metrics is not None:
@@ -212,7 +274,7 @@ class Sequential:
             # Training model :)
             self.__model.train()
 
-            # Spliting the data into batches
+            # Splitting the data into batches
             for i in range(0, len(x_train), batch_size):
                 # Making the batches
                 batch_x = x_train[i:i+batch_size].float()
@@ -259,12 +321,12 @@ class Sequential:
                     print_training_progress(
                         epoch, epochs, i, batch_size, len(x_train), train_loss.item())
 
-            # Evluating model
+            # Evaluating model
             self.__model.eval()
 
             # no grad, no training
             with torch.no_grad():
-                # Spliting the data into batches
+                # Splitting the data into batches
                 for i in range(0, len(x_test), batch_size):
                     # Making the batches
                     batch_x = x_train[i:i+batch_size].float()
@@ -302,14 +364,14 @@ class Sequential:
             # Calculating the mean val loss score for all batches
             validation_loss_score /= batch_size
 
-            # Added the epochwise value to the history dict
+            # Added the epochwise value to the history dictionary
             history["epochwise"]["training_loss"].append(training_loss_score)
             history["epochwise"]["validation_loss"].append(
                 validation_loss_score)
 
             # Checking if accuracy is there in metrics
             if "accuracy" in metrics:
-                # Adding data into hostory dict
+                # Adding data into history dictionary
                 history["epochwise"]["training_accuracy"].append(
                     correct_training/len(x_train)*100)
                 history["epochwise"]["training_accuracy"].append(
@@ -359,13 +421,14 @@ class Sequential:
         predictions = self.__predict(X, batch_size)
 
         # Converting to tensor
+        # pylint: disable=not-callable,no-member
         y_tensor = torch.tensor(y)
 
         # Calculating the loss
         loss = self.__loss_function(predictions, y_tensor)
 
         # if metrics has accuracy, then calculating accuracy
-        if "accuracy" in self.__metrics:
+        if self.__metrics and "accuracy" in self.__metrics:
             # Calculating no of corrects
             corrects = calculate_accuracy(y_tensor, predictions)
 
@@ -384,7 +447,7 @@ class Sequential:
         }
 
     def summary(self):
-        # Printing the model summary using pytorch model
+        # Printing the model summary using PyTorch model
         if self.__build:
             # Printing models summary
             print(self.__model)
@@ -400,13 +463,13 @@ class Sequential:
             raise Exception("You need to build the model first")
 
     def get_model(self):
-        # Returning the pytorch model
+        # Returning the PyTorch model
         return self.__model
 
     def set_model(self, model):
         # Checking if model is None
         if model is None:
-            raise ValueError("Please provide a valid pytorch model")
+            raise ValueError("Please provide a valid PyTorch model")
 
         # Saving the model
         self.__model = model
