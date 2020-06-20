3a62f49e0db9b31f087b9f58f55bd73be18ffdaa
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-06-20 23:07:32 +0530

    updated test scripts for mse

--- tests/neuralpy/loss_functions/test_mse.py
+++ tests/neuralpy/loss_functions/test_mse.py
@@ -5,23 +5,17 @@ from neuralpy.loss_functions import MSELoss
 import numpy as np
 import torch
 
-# Possible values that are invalid
-reductions=["asdas", "", 12, 6.3]
-
 @pytest.mark.parametrize(
 	"reduction", 
-	[(reduction) for reduction in reductions]
+	[("invalid", "", 12, 6.3)]
 )
 def test_cce_should_throw_value_error(reduction):
     with pytest.raises(ValueError) as ex:
         x = MSELoss(reduction=reduction)
 
-# Possible values that are valid
-reductions=["mean"]
-
 @pytest.mark.parametrize(
 	"reduction", 
-	[(reduction) for reduction in reductions]
+	[("none"), ("mean"), ("sum")]
 )
 def test_mse_get_layer_method(reduction):
 	x = MSELoss(reduction=reduction)
