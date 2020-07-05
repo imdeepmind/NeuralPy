62cb1202471f39b53ff9d1163f8b9003e15d98f8
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-05 18:29:31 +0530

    added avg pool 2d layer

---
+++ neuralpy/layers/avg_pool2d.py
@@ -0,0 +1,103 @@
+"""AvgPool2d layer for NeuralPy"""
+
+from torch.nn import AvgPool2d as _AvgPool2d
+
+
+class AvgPool2d:
+    """
+        Applies a 2D average pooling over an input signal composed of several input planes.
+
+        To learn more about Dense layers, please check PyTorch
+        documentation https://pytorch.org/docs/stable/nn.html?highlight=AvgPool2d#torch.nn.AvgPool2d
+
+        Supported Arguments:
+            start_dim: (Integer) first dim to AvgPool2d (default = 1)
+            end_dim: (Integer)  last dim to AvgPool2d (default = -1)
+    """
+
+    def __init__(self, kernel_size, stride=None, padding=0, ceil_mode=False, count_include_pad=True, divisor_override=True, name=None):
+        """
+            __init__ method for AvgPool2d
+
+            Supported Arguments:
+                start_dim: (Integer) first dim to AvgPool2d (default = 1)
+                end_dim: (Integer)  last dim to AvgPool2d (default = -1)
+
+        """
+        # Checking the kernel_size field
+        if not kernel_size or not (
+            isinstance(kernel_size, int) or isinstance(kernel_size, tuple)
+        ):
+            raise ValueError("Please provide a valid kernel_size")
+
+        # Checking the stride field
+        if stride is not None and not (
+            isinstance(stride, int) or isinstance(stride, tuple)
+        ):
+            raise ValueError("Please provide a valid stride")
+
+        # Checking the padding field
+        if padding is not None and not (
+            isinstance(padding, int) or isinstance(padding, tuple)
+        ):
+            raise ValueError("Please provide a valid padding")
+
+        # Checking ceil_mode
+        if not isinstance(ceil_mode, bool):
+            raise ValueError("Please provide a valid ceil_mode")
+
+        # Checking count_include_pad
+        if not isinstance(count_include_pad, bool):
+            raise ValueError("Please provide a valid count_include_pad")
+
+        # Checking divisor_override
+        if not isinstance(divisor_override, bool):
+            raise ValueError("Please provide a valid divisor_override")
+
+        # Checking the name field, this is an optional field,
+        # if not provided generates a unique name for the layer
+        if name is not None and not (isinstance(name, str) and name):
+            raise ValueError("Please provide a valid name")
+
+        # Storing the data
+        self.__kernel_size = kernel_size
+        self.__stride = stride
+        self.__padding = padding
+        self.__ceil_mode = ceil_mode
+        self.__count_include_pad = count_include_pad
+        self.__divisor_override = divisor_override
+
+        self.__name = name
+
+    def get_input_dim(self, prev_input_dim, prev_layer_type):
+        """
+            This method calculates the input shape for layer based on previous output layer.
+
+            This method is used by the NeuralPy Models, for building the models.
+            No need to call this method for using NeuralPy.
+        """
+        # AvgPool2d does not need to n_input, so returning None
+        return None
+
+    def get_layer(self):
+        """
+            This method returns the details as dict of the layer.
+
+            This method is used by the NeuralPy Models, for building the models.
+            No need to call this method for using NeuralPy.
+        """
+        # Returning all the details of the layer
+        return{
+            'layer_details': None,
+            'layer': _AvgPool2d,
+            'name': self.__name,
+            'type': 'AvgPool2d',
+            'keyword_arguments': {
+                'kernel_size': self.__kernel_size,
+                'stride': self.__stride,
+                'padding': self.__padding,
+                'ceil_mode': self.__ceil_mode,
+                'count_include_pad': self.__count_include_pad,
+                'divisor_override': self.__divisor_override
+            }
+        }
