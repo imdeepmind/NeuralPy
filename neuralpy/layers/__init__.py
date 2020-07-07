2798fa29e67c97a74af8a17895fe2c4f35fd5e48
Jeffin Francis <francisjeffin07@gmail.com>
2020-07-07 20:50:23 +0530

    test fix

--- neuralpy/layers/__init__.py
+++ neuralpy/layers/__init__.py
@@ -17,8 +17,11 @@ from .conv3d import Conv3D
 
 from .avg_pool2d import AvgPool2D
 
-from .rnn import RNN
-
 from .maxpool1d import MaxPool1d
 from .maxpool2d import MaxPool2d
 from .maxpool3d import MaxPool3d
+
+from .rnn import RNN
+from .gru import GRU
+from .lstm import LSTM
+
