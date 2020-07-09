74f133ccd7bee7a8902e5b303739ba6ce8fa2867
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-09 19:49:31 +0530

    completed test script for callbacks

--- tests/neuralpy/callbacks/test_train_logger.py
+++ tests/neuralpy/callbacks/test_train_logger.py
@@ -1,6 +1,8 @@
 import pytest
 from neuralpy.callbacks import TrainLogger
 
+# The test scripts may need to be updated
+
 def test_train_logger_should_throw_type_error():
     with pytest.raises(TypeError) as ex:
         x = TrainLogger()
@@ -10,3 +12,17 @@ def test_train_logger_should_throw_value_error(path):
     with pytest.raises(ValueError) as ex:
         x = TrainLogger(path=path)
 
+def test_train_logger_should_not_throw_any_error():
+    x = TrainLogger(path='ignore')
+    x.callback(10, 1, {}, {}, {})
+    x.callback(10, 
+                1, 
+                {
+                    'inplace': True
+                }, 
+                {
+                    'inplace': True
+                }, 
+                {
+                    'inplace': True
+                })
