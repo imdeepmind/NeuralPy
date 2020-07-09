1690519a444a51b81d77a3b71dc4a7a03fadbaac
Abhishek Chatterjee <abhishek.chatterjee97@protonmail.com>
2020-07-10 00:29:21 +0530

    Fixed maxpool3d

--- neuralpy/layers/conv3d.py
+++ neuralpy/layers/conv3d.py
@@ -214,7 +214,7 @@ class Conv3D:
 
             # based on the prev layer type, predicting the __input_shape
             # to support more layers, we need to add some more statements
-            if layer_type in ("conv3d", "avgpool3d", "maxpool3d):
+            if layer_type in ("conv3d", "avgpool3d", "maxpool3d"):
                 self.__input_shape = prev_input_dim[2]
             else:
                 raise ValueError(
