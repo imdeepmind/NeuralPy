779383a022b658442b3c0716cb782a1125a4c862
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-09 23:05:40 +0530

    cleaned the code for conv1d

--- neuralpy/layers/conv1d.py
+++ neuralpy/layers/conv1d.py
@@ -17,19 +17,20 @@ class Conv1D:
                 no need for this argument layers except the initial layer.
             stride: (Int | Tuple) Controls the stride for the cross-correlation, a single
                     number or a one-element tuple.
-            padding: (Int | Tuple) Controls the amount of implicit zero-paddings on both 
+            padding: (Int | Tuple) Controls the amount of implicit zero-paddings on both
                         sides for padding number of points
-            dilation: (Int | Tuple) Controls the spacing between the kernel points; also 
-                        known as the à trous algorithm. It is harder to describe, but this link has 
+            dilation: (Int | Tuple) Controls the spacing between the kernel points; also
+                        known as the à trous algorithm. It is harder to describe, but this link has
                         a nice visualization of what dilation does.
-            groups: (Int) Controls the connections between inputs and outputs. 
+            groups: (Int) Controls the connections between inputs and outputs.
                     input channel and filters must both be divisible by groups
             bias: (Boolean) If true then uses the bias, Defaults to `true`
             name: (String) Name of the layer, if not provided then
                 automatically calculates a unique name for the layer
     """
-
-    def __init__(self, filters, kernel_size, input_shape=None, stride=1, padding=0, dilation=1, groups=1, bias=True, name=None):
+    #pylint: disable=too-many-instance-attributes,too-many-arguments,too-many-branches
+    def __init__(self, filters, kernel_size, input_shape=None,
+                 stride=1, padding=0, dilation=1, groups=1, bias=True, name=None):
         """
             __init__ method for the Conv1D layer
 
@@ -40,12 +41,12 @@ class Conv1D:
                     no need for this argument layers except the initial layer.
                 stride: (Int | Tuple) Controls the stride for the cross-correlation, a single
                         number or a one-element tuple.
-                padding: (Int | Tuple) Controls the amount of implicit zero-paddings on both 
+                padding: (Int | Tuple) Controls the amount of implicit zero-paddings on both
                             sides for padding number of points
-                dilation: (Int | Tuple) Controls the spacing between the kernel points; also 
-                            known as the à trous algorithm. It is harder to describe, but this link has 
-                            a nice visualization of what dilation does.
-                groups: (Int) Controls the connections between inputs and outputs. 
+                dilation: (Int | Tuple) Controls the spacing between the kernel points; also
+                            known as the à trous algorithm. It is harder to describe,
+                            but this link has a nice visualization of what dilation does.
+                groups: (Int) Controls the connections between inputs and outputs.
                         input channel and filters must both be divisible by groups
                 bias: (Boolean) If true then uses the bias, Defaults to `true`
                 name: (String) Name of the layer, if not provided then
@@ -56,29 +57,27 @@ class Conv1D:
             raise ValueError("Please provide a valid filters")
 
         # Checking the kernel_size field
-        if not kernel_size or not (
-            isinstance(kernel_size, int) or isinstance(kernel_size, tuple)
-        ):
+        if kernel_size is not None and not isinstance(kernel_size, (int, tuple)):
             raise ValueError("Please provide a valid kernel_size")
 
         if isinstance(kernel_size, tuple):
             if not isinstance(kernel_size[0], int):
                 raise ValueError("Please provide a valid kernel_size")
-        
+
         # Checking the input_shape field, it is a optional field
         if input_shape is not None and not isinstance(input_shape, tuple):
             raise ValueError("Please provide a valid input_shape")
 
-        if input_shape is not None and not (isinstance(input_shape[0], int) and input_shape[0] >= 0):
+        if input_shape is not None and not (isinstance(input_shape[0], int)
+                                            and input_shape[0] >= 0):
             raise ValueError("Please provide a valid input_shape")
 
-        if input_shape is not None and not (isinstance(input_shape[1], int) and input_shape[1] >= 0):
+        if input_shape is not None and not (isinstance(input_shape[1], int)
+                                            and input_shape[1] >= 0):
             raise ValueError("Please provide a valid input_shape")
 
         # Checking the stride field
-        if stride is not None and not (
-            isinstance(stride, int) or isinstance(stride, tuple)
-        ):
+        if stride is not None and not isinstance(stride, (int, tuple)):
             raise ValueError("Please provide a valid stride")
 
         if isinstance(stride, tuple):
@@ -86,9 +85,7 @@ class Conv1D:
                 raise ValueError("Please provide a valid stride")
 
         # Checking the padding field
-        if padding is not None and not (
-            isinstance(padding, int) or isinstance(padding, tuple)
-        ):
+        if padding is not None and not isinstance(padding, (int, tuple)):
             raise ValueError("Please provide a valid padding")
 
         if isinstance(padding, tuple):
@@ -96,9 +93,7 @@ class Conv1D:
                 raise ValueError("Please provide a valid padding")
 
         # Checking the dilation field
-        if dilation is not None and not (
-            isinstance(dilation, int) or isinstance(dilation, tuple)
-        ):
+        if dilation is not None and not isinstance(dilation, (int, tuple)):
             raise ValueError("Please provide a valid dilation")
 
         if isinstance(dilation, tuple):
@@ -133,38 +128,39 @@ class Conv1D:
     def __get_layer_details(self):
         # Return tuple structure
         # Getting the kernel values
-        k1 = 0
+        kernel_1 = 0
         if isinstance(self.__kernel_size, int):
-            k1 = self.__kernel_size
+            kernel_1 = self.__kernel_size
         else:
-            k1, = self.__kernel_size
+            kernel_1, = self.__kernel_size
 
         # Getting the padding values
-        p1 = 0
+        padding_1 = 0
         if isinstance(self.__padding, int):
-            p1 = self.__padding
+            padding_1 = self.__padding
         else:
-            p1, = self.__padding
+            padding_1, = self.__padding
 
         # Getting the stride values
-        s1 = 0
+        stride_1 = 0
         if isinstance(self.__stride, int):
-            s1 = self.__stride
+            stride_1 = self.__stride
         else:
-            s1, = self.__stride
+            stride_1, = self.__stride
 
         # Getting the dilation  values
-        d1 = 0
+        dilation_1 = 0
         if isinstance(self.__dilation, int):
-            d1 = self.__dilation
+            dilation_1 = self.__dilation
         else:
-            d1, = self.__dilation
+            dilation_1, = self.__dilation
 
         # Calculating the width and height of the conv output
-        w1 = ((self.__input_shape[1] + 2 * p1 - d1 * (k1 - 1) - 1) // s1) + 1
+        dim_1 = ((self.__input_shape[1] + 2 * padding_1 -
+                  dilation_1 * (kernel_1 - 1) - 1) // stride_1) + 1
 
         # Returning for the next layers
-        return (self.__input_shape[0], w1*self.__filters, (self.__filters, w1))
+        return (self.__input_shape[0], dim_1*self.__filters, (self.__filters, dim_1))
 
     def get_input_dim(self, prev_input_dim, prev_layer_type):
         """
@@ -179,7 +175,7 @@ class Conv1D:
 
             # based on the prev layer type, predicting the __input_shape
             # to support more layers, we need to add some more statements
-            if layer_type == "conv1d" or layer_type == 'avgpool1d':
+            if layer_type in ("conv1d", 'avgpool1d'):
                 self.__input_shape = prev_input_dim[2]
             else:
                 raise ValueError(
