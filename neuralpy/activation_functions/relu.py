2c9fe6aaa7d692a375986cec47e0a491d25e1251
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-10-20 00:00:27 +0530

    made the changes in ReLU

--- neuralpy/activation_functions/relu.py
+++ neuralpy/activation_functions/relu.py
@@ -1,54 +1,52 @@
 """ReLU Activation Function"""
 
 from torch.nn import ReLU as _ReLU
-from .utils import get_activation_details, validate_name_field
+from neuralpy.utils import CustomLayer
 
 
-class ReLU:
+class ReLU(CustomLayer):
+  """
+    ReLU applies a rectified linear unit function to the input tensors.
+
+    For more information, check https://pytorch.org/docs/stable/nn.html#relu
+
+    Supported Arguments
+        name=None: (String) Name of the activation function layer,
+            if not provided then automatically calculates a unique name for the layer
+  """
+
+  def __init__(self, name=None):
     """
-        ReLU applies a rectified linear unit function to the input tensors.
+      __init__ method for the ReLU Activation Function class
 
-        For more information, check https://pytorch.org/docs/stable/nn.html#relu
-
-        Supported Arguments
-            name=None: (String) Name of the activation function layer,
-                if not provided then automatically calculates a unique name for the layer
+      Supported Arguments
+          name=None: (String) Name of the activation function layer,
+              if not provided then automatically calculates a unique name for the layer
     """
+    # Checking the name field, this is an optional field,
+    # if not provided generates a unique name for the activation function
+    super().__init__(_ReLU, "ReLU", layer_name=name)
 
-    def __init__(self, name=None):
-        """
-            __init__ method for the ReLU Activation Function class
+  # pylint: disable=W0613, disable=R0201
+  def get_input_dim(self, prev_input_dim, layer_type):
+    """
+      This method calculates the input shape for layer based on previous output layer.
+      Here for this activation function, we dont need it
 
-            Supported Arguments
-                name=None: (String) Name of the activation function layer,
-                    if not provided then automatically calculates a unique name for the layer
-        """
-        # Checking the name field, this is an optional field,
-        # if not provided generates a unique name for the activation function
-        validate_name_field(name)
+      This method is used by the NeuralPy Models, for building the models.
+      No need to call this method for using NeuralPy.
+    """
+    # ReLU does not need to n_input, so returning None
+    return None
 
-        self.__name = name
+  def get_layer(self):
+    """
+      Provides details of the layer
 
-    # pylint: disable=W0613, disable=R0201
-    def get_input_dim(self, prev_input_dim, layer_type):
-        """
-            This method calculates the input shape for layer based on previous output layer.
-            Here for this activation function, we dont need it
-
-            This method is used by the NeuralPy Models, for building the models.
-            No need to call this method for using NeuralPy.
-        """
-        # ReLU does not need to n_input, so returning None
-        return None
-
-    def get_layer(self):
-        """
-            Provides details of the layer
-
-            This method is used by the NeuralPy Models, for building the models.
-            No need to call this method for using NeuralPy.
-        """
-        # Returning all the details of the activation function
-        return get_activation_details(self.__name, 'ReLU', _ReLU, {
-            'inplace': False
-        })
+      This method is used by the NeuralPy Models, for building the models.
+      No need to call this method for using NeuralPy.
+    """
+    # Returning all the details of the activation function
+    return self._get_layer_details(None, {
+      'inplace': True
+    })
