f5a77ecdf8b8c65b8641ad6805ff87bb8908c503
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-10-20 00:10:35 +0530

    updated sigmoid

--- neuralpy/activation_functions/sigmoid.py
+++ neuralpy/activation_functions/sigmoid.py
@@ -1,51 +1,46 @@
 """Sigmoid Activation Function"""
 
 from torch.nn import Sigmoid as _Sigmoid
-from .utils import get_activation_details, validate_name_field
+from neuralpy.utils import CustomLayer
 
 
-class Sigmoid:
+class Sigmoid(CustomLayer):
+  """
+    Applies a element-wise Sigmoid or Logistic function to the input tensor.
+
+    Supported Arguments
+      name=None: (String) Name of the activation function layer,
+          if not provided then automatically calculates a unique name for the layer.
+  """
+
+  def __init__(self, name=None):
     """
-        Applies a element-wise Sigmoid or Logistic function to the input tensor.
+      __init__ method for the Sigmoid Activation Function class
 
-        Supported Arguments
-            name=None: (String) Name of the activation function layer,
-                if not provided then automatically calculates a unique name for the layer.
+      Supported Arguments
+        name=None: (String) Name of the activation function layer,
+            if not provided then automatically calculates a unique name for the layer
     """
+    super().__init__(_Sigmoid, "Sigmoid", layer_name=name)
 
-    def __init__(self, name=None):
-        """
-            __init__ method for the Sigmoid Activation Function class
+  # pylint: disable=W0613, disable=R0201
+  def get_input_dim(self, prev_input_dim, layer_type):
+    """
+      This method calculates the input shape for layer based on previous output layer.
+      Here for this activation function, we dont need it
 
-            Supported Arguments
-                name=None: (String) Name of the activation function layer,
-                    if not provided then automatically calculates a unique name for the layer
-        """
-        # Checking the name field,
-        # this is an optional field,
-        # if not provided generates a unique name for the activation function
-        validate_name_field(name)
+      This method is used by the NeuralPy Models, for building the models.
+      No need to call this method for using NeuralPy.
+    """
+    # Sigmoid does not need to n_input, so returning None
+    return None
 
-        self.__name = name
+  def get_layer(self):
+    """
+      Provides details of the layer
 
-    # pylint: disable=W0613, disable=R0201
-    def get_input_dim(self, prev_input_dim, layer_type):
-        """
-            This method calculates the input shape for layer based on previous output layer.
-            Here for this activation function, we dont need it
-
-            This method is used by the NeuralPy Models, for building the models.
-            No need to call this method for using NeuralPy.
-        """
-        # Sigmoid does not need to n_input, so returning None
-        return None
-
-    def get_layer(self):
-        """
-            Provides details of the layer
-
-            This method is used by the NeuralPy Models, for building the models.
-            No need to call this method for using NeuralPy.
-        """
-        # Returning all the details of the activation function
-        return get_activation_details(self.__name, 'Sigmoid', _Sigmoid, None)
+      This method is used by the NeuralPy Models, for building the models.
+      No need to call this method for using NeuralPy.
+    """
+    # Returning all the details of the activation function
+    return self._get_layer_details()
