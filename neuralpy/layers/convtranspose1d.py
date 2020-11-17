ac1b038cc0ab0798b01da51059c26482aa4b368e
jeffin07 <francisjeffin07@gmail.com>
2020-11-17 22:18:58 +0530

    completed convtranspose1d tests

--- neuralpy/layers/convtranspose1d.py
+++ neuralpy/layers/convtranspose1d.py
@@ -5,7 +5,21 @@ from torch.nn import ConvTranspose1d as _ConvTranspose1d
 
 class ConvTranspose1d:
     """
+        Applies a 1D transposed convolution operator over an input image composed of several input planes.
 
+        To learn more about ConvTranspose1D layers, please check PyTorch
+        documentation at https://pytorch.org/docs/stable/generated/torch.nn.ConvTranspose1d.html?highlight=convtr#torch.nn.ConvTranspose1d
+
+        Supported Arguments:            
+            in_channels: (Integer) Number of channels in the input image
+            out_channels: (Integer)  Number of channels produced by the convolution
+            kernel_size: (Integer | Tuple)  Size of the convolving kernel
+            stride: (Integer | Tuple)  Stride of the convolution. Default: 1
+            padding: (Integer | Tuple)  dilation * (kernel_size - 1) - padding zero-padding will be added to both sides of the input. Default: 0
+            output_padding: (Integer | Tuple)  Additional size added to one side of the output shape. Default: 0
+            groups: (Integer)  Number of blocked connections from input channels to output channels. Default: 1
+            bias: (Boolean)  If True, adds a learnable bias to the output. Default: True
+            dilation: (Integer | Tuple)  Spacing between kernel elements. Default: 1
     """
 
     def __init__(self, in_channels, out_channels, kernel_size,
@@ -13,44 +27,54 @@ class ConvTranspose1d:
                 output_padding=0, groups=1, bias=True,
                 dilation=1, name=None):
         
-
+        # Checking the in_channels field
         if not in_channels or not isinstance(
                 in_channels, int) or in_channels < 0:
             raise ValueError("Please provide a valid in_channels")
 
+        # Checking the out_channels field
         if not out_channels or not isinstance(
                 out_channels, int) or out_channels < 0:
             raise ValueError("Please provide a valid out_channels")
 
+        # Checking the kernel_size field
         if not kernel_size or not isinstance(kernel_size, (int, tuple)):
             raise ValueError("Please provide a valid kernel_size")
 
+        # Checking the input_shape field, it is a optional field
         if input_shape is not None and not (isinstance(input_shape[1], int)
                 and input_shape[1] >= 0):
             raise ValueError("Please provide a valid input_shape")
 
+        # Checking the stride field, it is a optional field
         if stride is not None and not isinstance(stride, (int, tuple)):
             raise ValueError("Please provide a valid stride")
 
+        # Checking the padding field, it is a optional field
         if padding is not None and not isinstance(padding, (int, tuple)):
             raise ValueError("Please provide a valid padding")
 
-        if output_padding is not None and not isinstance(output_padding, int, tuple):
+        # Checking the output_padding field, it is a optional field
+        if output_padding is not None and not isinstance(output_padding, (int, tuple)):
             raise ValueError("Please provide a valid output_padding")
 
+        # Checking the groups field, it is a optional field
         if groups is not None and not isinstance(groups, int):
             raise ValueError("Please provide a valid groups")
 
+        # Checking the bias field, it is a optional field
         if bias is not None and not isinstance(bias, bool):
             raise ValueError("Please provide a valid bias")
 
-        if dilation is not None and not isinstance(dilation, isinstance(dilation, (int, tuple))):
+        # Checking the dilation field, it is a optional field
+        if dilation is not None and not isinstance(dilation, (int, tuple)):
             raise ValueError("Please provide a valid dilation")
 
+        # Checking the name field, it is a optional field
         if name is not None and not (isinstance(name, str) and name):
             raise ValueError("Please provide a valid name")
 
-
+        # Storing the data
         self.__in_channels = in_channels
         self.__out_channels = out_channels
         self.__kernel_size = kernel_size
@@ -65,52 +89,78 @@ class ConvTranspose1d:
         self.__name = name
 
     def __get_layer_details(self):
-
+        # Return tuple structure
+        # Getting the kernel values
         kernel1 = 0
         if isinstance(self.__kernel_size, int):
             kernel1 = self.__kernel_size
         else:
             kernel1, = self.__kernel_size
 
+        # Getting the padding values
         padding1 = 0
         if isinstance(self.__padding, int):
             padding1 = self.__padding
         else:
             padding1, = self.__padding
         
+        # Getting the stride values
+        stride_1 = 0
+        if isinstance(self.__stride, int):
+            stride_1 = self.__stride
+        else:
+            stride_1, = self.__stride
+        
+        # Getting the dilation values
         dilation1 = 0
         if isinstance(self.__dilation, int):
             dilation1  = self.__dilation
         else:
             dilation1, = self.__dilation
         
+        # Getting the out_padding values
         out_padding1 = 0
         if isinstance(self.__out_padding, int):
             out_padding1 = self.__out_padding
         else:
             out_padding1, = self.__out_padding
         
-        
-        dim1 = ((self.__input_shape[-1] -1) * (self.__stride -2) * self.__padding + self.__dilation * (self.__kernel_size -1) + (self.__out_padding + 1))
+        # Calculating the width and height of the convtranspose output
+        dim1 = ((self.__input_shape[-1] -1) * (stride_1 -2) * padding1 + dilation1 * (kernel1 -1) + (out_padding1 + 1))
 
         return(self.__input_shape[0], self.__in_channels*dim1, (self.__in_channels, dim1))
 
     def get_input_dim(self, prev_input_dim, prev_layer_type):
-        
+        """
+            This method calculates the input shape for layer based on previous output layer.
+
+            This method is used by the NeuralPy Models, for building the models.
+            No need to call this method for using NeuralPy.
+        """
+        # Checking if n_inputs is there or not, not overwriting the n_input field
         if not self.__input_shape:
             layer_type = prev_layer_type.lower()
 
+            # based on the prev layer type, predicting the __input_shape
+            # to support more layers, we need to add some more statements
             if layer_type == 'convtranspose1d':
                 self.__input_shape = prev_input_dim[-1]
             else:
                 raise ValueError("Unsupported previous layer, please provide your own input shape for the layer")
 
     def get_layer(self):
+         """
+            This method returns the details as dict of the layer.
+
+            This method is used by the NeuralPy Models, for building the models.
+            No need to call this method for using NeuralPy.
+        """
+        # Returning all the details of the layer
 
         return{
                 'layer_details': self.__get_layer_details(),
                 'name': self.__name,
-                'type': 'ConvTranspose1d',
+                'type': 'ConvTranspose1D',
                 'layer': _ConvTranspose1d,
                 'keyword_arguments':{
                         'in_channels': self.__in_channels,
--- tests/neuralpy/layers/test_convtranspose1d.py
+++ tests/neuralpy/layers/test_convtranspose1d.py
@@ -13,72 +13,134 @@ def test_convtranspose1d_should_throw_type_error():
     "in_channels, out_channels, kernel_size,input_shape, stride, padding, output_padding, groups, bias, dilation,name",
     [
         #Checking in_channels validation
-        (0.3,0.3,0.36,"invalid","invalid","invalid","invalid","groups",False,"invalid",""),
-        (0.3,0.3,0.36,"invalid","invalid","invalid","invalid","groups",False,"invalid",""),
+        (0.3, 0.3, 0.36, "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (0.3, 0.3, 0.36, "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
 
         #Checking out_channels validation
-        (3,3,0.36,"invalid","invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,False,0.36,"invalid","invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,"",0.36,"invalid","invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,("",),0.36,"invalid","invalid","invalid","invalid","groups",False,"invalid",""),
+        (3, 3, 0.36, "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, False, 0.36, "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, "", 0.36, "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, ("",), 0.36, "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
 
         #Checking kernel_size validation
-        (3,(3,),"invalid","invalid","invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),False,5.7,"invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),(3,""),"invalid","invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),("",3),"invalid","invalid","invalid","invalid","groups",False,"invalid",""),
+        (3, (3,), "invalid", "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, (3,), False, 5.7, "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, (3,), (3,""), "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, (3,), ("",3), "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
 
         #Checking input_shape validation
-        (3,(3,),(3,3),"invalid","invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),5.7,"invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),False,"invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),("",),"invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),(5.7,),"invalid","invalid","invalid","groups",False,"invalid",""),
+        (3, (3,), (3,3), "invalid", "invalid", "invalid", "invalid", "groups", False, "invalid",""),
+        (3, (3,), (3,3), 5.7, "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, (3,), (3,3), False, "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, (3,), (3,3), ("",), "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, (3,), (3,3), (5.7,), "invalid", "invalid", "invalid", "groups", False, "invalid", ""),
 
         #Checking stride validation
-        (3,(3,),(3,3),(3,),"invalid","invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),False,"invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),5.7,"invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),("",),"invalid","invalid","groups",False,"invalid",""),
+        (3, (3,), (3,3), (3,), "invalid", "invalid", "invalid","groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), False, "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), 5.7, "invalid", "invalid", "groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), ("",), "invalid", "invalid", "groups", False, "invalid", ""),
 
         #Checking padding validation
-        (3,(3,),(3,3),(3,),(3,3),"invalid","invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),False,"invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),5.7,"invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),("",),"invalid","groups",False,"invalid",""),
+        (3, (3,), (3,3), (3,), (3,3), "invalid", "invalid","groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), False, "invalid", "groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), 5.7, "invalid", "groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), ("",), "invalid", "groups", False, "invalid", ""),
 
         #Checking output_padding validation
-        (3,(3,),(3,3),(3,),(3,3),(3,),"invalid","groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),(3,),False,"groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),3,5.7,"groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),(3,3),("",),"groups",False,"invalid",""),
+        (3, (3,), (3,3), (3,), (3,3), (3,), "invalid", "groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), (3,), False, "groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), 3,5.7, "groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), (3,3), ("",), "groups", False, "invalid", ""),
 
         #Checking groups validation
-        (3,(3,),(3,3),(3,),(3,3),(3,),(3,3),"groups",False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),(3,),(3,3),1,False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),3,(3,3),5.7,False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),(3,3),(3,3),("",),False,"invalid",""),
+        (3, (3,), (3,3), (3,), (3,3), (3,), (3,3), "groups", False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), (3,), (3,3), 1, False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), 3, (3,3), 5.7, False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), (3,3), (3,3), ("",), False, "invalid", ""),
 
         #Checking bias validation
-        (3,(3,),(3,3),(3,),(3,3),(3,),(3,3),3,"invalid","invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),(3,),(3,3),3,False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),3,(3,3),3,3,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),(3,3),(3,3),3,5.7,"invalid",""),
+        (3, (3,), (3,3), (3,), (3,3), (3,), (3,3), 3, "invalid", "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), (3,), (3,3), 3, False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), 3, (3,3) , 3, 3, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), (3,3), (3,3), 3, 5.7, "invalid", ""),
 
         #Checking dilation validation
-        (3,(3,),(3,3),(3,),(3,3),(3,),(3,3),3,False,"invalid",""),
-        (3,(3,),(3,3),(3,),(3,3),(3,),(3,3),3,False,3,""),
-        (3,(3,),(3,3),(3,),(3,3),3,(3,3),3,3,("",),""),
-        (3,(3,),(3,3),(3,),(3,3),(3,3),(3,3),3,5.7,5.7,""),
+        (3, (3,), (3,3), (3,), (3,3), (3,), (3,3), 3, False, "invalid", ""),
+        (3, (3,), (3,3), (3,), (3,3), (3,), (3,3), 3, False, 3, ""),
+        (3, (3,), (3,3), (3,), (3,3), 3, (3,3), 3,3, ("",), ""),
+        (3, (3,), (3,3), (3,), (3,3), (3,3), (3,3), 3, 5.7, 5.7, ""),
 
         #Checking dilation validation
-        (3,(3,),(3,3),(3,),(3,3),(3,),(3,3),3,False,(3,),"invalid"),
-        (3,(3,),(3,3),(3,),(3,3),(3,),(3,3),3,False,(3,),False),
-        (3,(3,),(3,3),(3,),(3,3),3,(3,3),3,3,(3,),3.5),
-        (3,(3,),(3,3),(3,),(3,3),(3,3),(3,3),3,5.7,(3,),3),
+        (3, (3,), (3,3), (3,), (3,3), (3,), (3,3), 3, False, (3,), "invalid"),
+        (3, (3,), (3,3), (3,), (3,3), (3,) ,(3,3), 3, False, (3,), False),
+        (3, (3,), (3,3), (3,), (3,3), 3, (3,3) ,3, 3, (3,), 3.5),
+        (3, (3,), (3,3), (3,), (3,3), (3,3), (3,3), 3, 5.7, (3,), 3),
+
+        #Checking name validations
+        (3, (3,), (3,3), (3,), (3,3), (3,), (3,3), 3, False, (3,), ""),
+        (3, (3,), (3,3), (3,), (3,3), (3,), (3,3), 3, False, (3,), 12)
 
     ]
 )
 def test_convtrans1d_should_throw_value_error(in_channels, out_channels, kernel_size, input_shape,stride,padding, output_padding,groups,bias,dilation,name):
     with pytest.raises(ValueError) as ex:
         x = ConvTranspose1d(in_channels=in_channels, out_channels=out_channels,kernel_size=kernel_size, input_shape=input_shape, stride=stride, padding=padding, output_padding=output_padding,groups=groups,bias=bias,dilation=dilation, name=name)
+
+
+in_channels = [4]
+out_channels = [6]
+kernel_size = [3, (3,)]
+input_shape = [(4, 5), None]
+stride = [3, (3,)]
+padding = [3, (3,)]
+output_padding = [2, (2,)]
+groups = [1]
+bias = [True]
+dilation = [3, (3,)]
+name = ["Name", None]
+
+@pytest.mark.parametrize(
+    "_in_channels, _out_channels, _kernel_size, _input_shape, _stride, _padding, _out_padding, _groups, _bias, _dilation, _name",
+    [(_in_channels, _out_channels, _kernel_size, _input_shape, _stride, _padding, _out_padding, _groups, _bias, _dilation, _name)
+     for _in_channels in in_channels
+     for _out_channels in out_channels
+     for _kernel_size in kernel_size
+     for _input_shape in input_shape
+     for _stride in stride
+     for _padding in stride
+     for _out_padding in output_padding
+     for _groups in groups
+     for _bias in bias
+     for _dilation in dilation
+     for _name in name]
+)
+def test_convtrans1d_get_layer_method(_in_channels, _out_channels, _kernel_size, _input_shape, _stride, _padding, _out_padding, _groups, _bias, _dilation, _name):
+    x = ConvTranspose1d(in_channels=_in_channels, out_channels=_out_channels, kernel_size=_kernel_size,
+                input_shape=_input_shape, stride=_stride, padding=_padding,
+                output_padding=_out_padding, groups=_groups, bias=_bias,
+                dilation=_dilation, name=_name)
+
+    if _input_shape is None:
+        prev_dim = (3, 3*32, (3,32))
+        x.get_input_dim(prev_dim, "convtranspose1d")
+
+    details = x.get_layer()
+
+    assert isinstance(details, dict) == True
+
+    assert details["name"] == _name
+
+    assert issubclass(details["layer"], _ConvTranspose1d) == True
+
+    assert details["type"] == "ConvTranspose1D"
+
+    assert isinstance(details["keyword_arguments"], dict) == True
+
+def test_convtrans1d_get_layer_method_invalid_layer():
+    x = ConvTranspose1d(in_channels=16, out_channels=12, kernel_size=2, input_shape=None)
+
+    prev_dim = (3, 3*32, (3, 32))
+
+    with pytest.raises(ValueError) as ex:
+        x.get_input_dim(prev_dim, "conv2d")
