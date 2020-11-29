3f59373f5d461651652d6fc74594b3b71b53d701
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-11-29 22:51:35 +0530

    fixed issues with imports

--- neuralpy/layers/__init__.py
+++ neuralpy/layers/__init__.py
@@ -1,8 +1,8 @@
-"""Layers are the building blocks of a Neural Network.
-A complete Neural Network model consists of several layers.
-A Layer is a function that receives a tensor as output,
-computes something out of it, and finally outputs a tensor.
-
+"""
+    Layers are the building blocks of a Neural Network.
+    A complete Neural Network model consists of several layers.
+    A Layer is a function that receives a tensor as output,
+    computes something out of it, and finally outputs a tensor.
 """
 
 from . import convolutional
