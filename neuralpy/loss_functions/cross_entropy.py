32139a0c38e47ee2c819e84291cc1a64fc615b34
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-12-05 15:51:19 +0530

    some minor tweks for the loss functions

--- neuralpy/loss_functions/cross_entropy.py
+++ neuralpy/loss_functions/cross_entropy.py
@@ -2,9 +2,7 @@
 
 import numpy as np
 import torch
-from torch.nn import CrossEntropyLoss as _CrossEntrypyLoss
-
-# pylint: disable=too-few-public-methods
+from torch.nn import CrossEntropyLoss as _CrossEntropyLoss
 
 
 class CrossEntropyLoss:
@@ -55,16 +53,14 @@ class CrossEntropyLoss:
             Sequential model to build the model
         """
         # If weight provided, then converting it into torch tensor
-        # pylint: disable=not-callable
         weight = None
 
         if self.__weight is not None:
             weight = torch.tensor(self.__weight).float()
 
         return {
-            'loss_function': _CrossEntrypyLoss,
+            'loss_function': _CrossEntropyLoss,
             'keyword_arguments': {
-                # pylint: disable=not-callable
                 'weight': weight,
                 'ignore_index': self.__ignore_index,
                 'reduction': self.__reduction
